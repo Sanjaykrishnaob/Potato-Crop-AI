309dcb58f96c315c19d426a22329359408d40d3e
Sanjay Krishna OB <168282996+Sanjaykrishnaob@users.noreply.github.com>
2025-09-06 12:08:41 +0530

    Update config.js

--- web_dashboard/src/firebase/config.js
+++ web_dashboard/src/firebase/config.js
@@ -8,13 +8,7 @@ import { getAnalytics } from 'firebase/analytics';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAu9syd2BkJ4cJPnln5S_2seVBU6Rpp0nI",
-  authDomain: "potato-crop-ai.firebaseapp.com",
-  projectId: "potato-crop-ai",
-  storageBucket: "potato-crop-ai.firebasestorage.app",
-  messagingSenderId: "359161107115",
-  appId: "1:359161107115:web:c71c57a1cf193d285d5dee",
-  measurementId: "G-J9R3L0NDWB"
+ "Paste Firebase Config Json Here"
 };
 
 // Initialize Firebase
